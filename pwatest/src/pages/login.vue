e991462a77ad9917eb8e75c68650574a7798b0d2
Mário Roberto <50268227+MarioRobertoRonca@users.noreply.github.com>
2020-10-18 14:50:32 -0300

    Primeiro commit

---
+++ pwatest/src/pages/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page class=" constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width"
+      src="https://cdn.quasar.dev/img/parallax2.jpg">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn color="grey-10" icon="eva-camera" size="lg" round />
+      <div class="row justify-center">
+        <div class="col-12 col-md-6 q-mt-xl">
+          <q-input outlined v-model="text" label="CPF" />
+          <div class="col-12 col-md-6 q-mt-md">
+            <q-input outlined v-model="text" type="password" label="Senha" />
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-center q-mt-lg" >
+        <q-btn unelevated
+          color="primary"
+          label="Login"
+          rounded
+          style="width:200px"
+          to="/camera"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageCamera",
+};
+</script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
---
+++ pwatest/src/pages/pageCamera.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width"
+      src="https://cdn.quasar.dev/img/parallax2.jpg">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+        round
+      />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="text"
+          class="col"
+          label="caption"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="text"
+          class="col"
+          label="location"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn
+              icon="eva-navigation-2-outline"
+              round
+              dense
+              flat
+               />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn
+          unelevated
+          color="primary"
+          label="Post Image"
+          rounded
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageCamera'
+}
+</script>
+
+<style lang="sass">
+    .camera-frame
+      border: 2px solid $grey-10
+      border-radius: 10px
+</style>
