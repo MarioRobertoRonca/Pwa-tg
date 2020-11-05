2b3a2aed0c9d047d7a72882af605961ecdaf3ebb
Paloma Faria <54600137+palomafaria@users.noreply.github.com>
2020-11-05 18:06:44 -0300

    Inicio creatAccount 05-11-2020

--- pwatest/src/pages/createAccount.vue
+++ pwatest/src/pages/createAccount.vue
@@ -1,8 +1,51 @@
 <template>
+  <div class="q-gutter-md q-px-lg">
+    <div class="constrain-more-avatar q-pa-xl justify-center">
+      <q-avatar size="150px">
+        <img
+          style="position: center"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSaj0ucKVpTNbey2YUj2f0V_MDQ1G6jBiwt2w&usqp=CAU"
+        />
+      </q-avatar>
+    </div>
+
+    <!--Formulário de Cadastro -->
+    <!--Nome Completo-->
+    <div class="row justify-center q-ma-md">
+      <q-input class="input" color= "#45e" bg-color="grey-3" filled v-model="text" :dense="dense" label= "Nome Completo" >
+          <template v-slot:prepend>
+            <q-icon name="person"/>
+          </template>
+        </q-input>
+
+    <!--CPF-->
+        <q-input class="input" color= "#45e" bg-color="grey-3" filled v-model="text" :dense="dense" label= "CPF" >
+          <template v-slot:prepend>
+            <q-icon name="person"/>
+          </template>
+        </q-input>
+    </div>
+
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data () {
+    return {
+      text: '',
+      ph: '',
+
+      dense: false
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+.input {
+  padding-bottom: 16px;
+  padding: 16px;
+  width: 400px;
+}
+</style>
--- pwatest/src/pages/login.vue
+++ pwatest/src/pages/login.vue
@@ -5,30 +5,34 @@
     <div class="q-pa-ma">
       <img class="full-width"
        src="https://lh3.googleusercontent.com/pw/ACtC-3cWlUvueLUYbSQUa_K5Zx87Fju-tYA3hDMH2wqnjsrL51XuzRD3dSyNvZ5IhCm6J0dl6JpX_6aTtLZHYoE8bcFtOA5-w5BtC7lyJTLhGupgAHNIjSM73tb40cPh8wL32c9Znuf8n9mO1FgP6IQd061y=s310-no?authuser=0">
-    </div>
 
-    <!--Icone Tesoura-->
+      <!--Icone Tesoura-->
     <div class="row justify-center q-pa-ma">
       <q-btn class="btn-icon" color="primary" icon="eva-scissors-outline" size="lg" round/>
     </div>
+    </div>
+
+    
 
     <!--Formulário de Login -->
       <div class="row justify-center q-ma-md" >
         <!--Input CPF-->
-        <q-input class="input" color= "#45e" bg-color="grey-3" filled v-model="cpf" label= "CPF" 
-        >
+        <q-input class="input" color= "#45e" bg-color="grey-3" filled v-model="text" label= "CPF" >
           <template v-slot:prepend>
             <q-icon name="person"/>
           </template>
         </q-input>
 
         <!--Input Password-->
-        <q-input class="input" color= "#45e" bg-color="grey-3" filled v-model="senha" label="Senha"
-        type="password">
-          <template v-slot:prepend>
-            <q-icon name="vpn_key"/>
-          </template>
-        </q-input>
+        <q-input class="input" color= "#45e" bg-color="grey-3" v-model="password" label ="Senha" filled :type="isPwd ? 'password' : 'text'" hint="Password with toggle">
+        <template v-slot:prepend>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
       </div>
 
       <!--Botão de Login-->
@@ -54,9 +58,18 @@ import { uid } from 'quasar'
 require('md-gum-polyfill')
 
 export default {
-  name: 'Login'
+  data () {
+    return {
+      text:'',
+      password: '',
+      isPwd: true,
+    }
+  }
 }
 </script>
+
+
+
 
 <style>
  .input{
